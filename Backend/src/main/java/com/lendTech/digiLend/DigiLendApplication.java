e54fc9999c4394aae9390a8cb123af877425e277
qortez <rodgernyangweso@gmail.com>
2022-03-01 21:27:26 +0300

    Initial commit

---
+++ Backend/src/main/java/com/lendTech/digiLend/DigiLendApplication.java
@@ -0,0 +1,13 @@
+package com.lendTech.digiLend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiLendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiLendApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/lendTech/digiLend/DigiLendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lendTech.digiLend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiLendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
