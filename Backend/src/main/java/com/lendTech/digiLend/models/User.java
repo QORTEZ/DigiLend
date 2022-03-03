bb6e7fada365beaf67685637d26e63bd661af507
qortez <rodgernyangweso@gmail.com>
2022-03-03 14:11:47 +0300

    Models and services for users and their authentication

---
+++ Backend/src/main/java/com/lendTech/digiLend/models/User.java
@@ -0,0 +1,57 @@
+package com.lendTech.digiLend.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username")
+        })
+@Setter
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String phoneNumber;
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    @JsonIgnore
+    private String password;
+
+    private String accountStatus;
+
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @JsonIgnore
+    private Set<Role> roles = new HashSet<>();
+
+    public User(String name, String username, String phoneNumber, String password) {
+        this.name = name;
+        this.username = username;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+}
