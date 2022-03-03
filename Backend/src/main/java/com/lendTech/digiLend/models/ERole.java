da5de83a2327750f277d830ef6ec229c6cc1d471
qortez <rodgernyangweso@gmail.com>
2022-03-03 14:02:43 +0300

    Added use roles for customer and admin

---
+++ Backend/src/main/java/com/lendTech/digiLend/models/ERole.java
@@ -0,0 +1,7 @@
+package com.lendTech.digiLend.models;
+
+public enum ERole {
+    ROLE_CUSTOMER,
+    ROLE_ADMIN
+
+}
---
+++ Backend/src/main/java/com/lendTech/digiLend/models/Role.java
@@ -0,0 +1,22 @@
+package com.lendTech.digiLend.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20)
+    private String name;
+}
