4e6e253f4ec02353bd643519ff252d6771eebd97
qortez <rodgernyangweso@gmail.com>
2022-03-05 11:55:31 +0300

    CORS handling extension to Controllers

--- Backend/src/main/java/com/lendTech/digiLend/controllers/LoansController.java
+++ Backend/src/main/java/com/lendTech/digiLend/controllers/LoansController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.text.ParseException;
 import java.util.List;
 
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/")
 public class LoansController {
