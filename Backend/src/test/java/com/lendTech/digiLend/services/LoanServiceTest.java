a5025bfef9f9c48ba39d2ab601b3a4bc3069d162
qortez <rodgernyangweso@gmail.com>
2022-03-05 11:38:32 +0300

    Global CORS handling added

---
+++ Backend/src/test/java/com/lendTech/digiLend/services/LoanServiceTest.java
@@ -0,0 +1,28 @@
+package com.lendTech.digiLend.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoanServiceTest {
+
+    @Test
+    void saveLoan() {
+    }
+
+    @Test
+    void getLoansByUserId() {
+    }
+
+    @Test
+    void getAllLoans() {
+    }
+
+    @Test
+    void getLoanLimitForUser() {
+    }
+
+    @Test
+    void applyLoan() {
+    }
+}
